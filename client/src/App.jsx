5cbaf377a452229e0524ffd7b4b2ba3d8a7a1dc8
komsak007 <98792390+k0msak007@users.noreply.github.com>
2023-11-16 13:19:11 +0700

    Create client and header component

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="*" element={<Navigate replace  to={"/"}/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
