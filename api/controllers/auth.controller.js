5c465a03723da5ccb5fd7ad572affcc6f9eb5b99
komsak007 <98792390+k0msak007@users.noreply.github.com>
2023-11-16 16:26:06 +0700

    create middleware and handler error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,16 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
-    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-  } catch (error) {
-    res.status(500).json(error);
-  }
+    res.status(201).json("User created successfully");
 
-  return res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error)
+  }
 };
